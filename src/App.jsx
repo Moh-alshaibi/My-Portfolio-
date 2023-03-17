56a3c735c5f38d44b0e05f5aece9021b6a6dbdc9
Moh-alshaibi <kib09005@gmail.com>
2023-03-17 18:54:24 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>Mo Code</div>
+        <nav>
+          <ul>
+          <li>files</li>
+          <li>files</li>
+        </ul>
+        </nav>
+      </div>
+      <div>
+      <div>Img</div>
+      <p>Text</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
