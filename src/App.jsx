17a50bf3a675c5997c04c233234e897168a5e62d
Moh-alshaibi <kib09005@gmail.com>
2023-03-20 23:16:58 +0100

    edited main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Nav from "./components/nav/Nav";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <div>
       <Nav />
-      <div>
-        <img src="" alt="Image of the creator of page" />
+      <div className="flex justify-center mt-64 ml-28">
+        <img src="./src/assets/img/me.png" alt="" className="" />
+        <div>
+          <p className="ml-12 mt-4">Hey!</p>
+          <p className="ml-12 mt-4 w-96 ">My name is <span className="text-red-900">Mohamed</span> and I'm a web developer who specializes in designing and building web applications and websites.</p>
+        </div>
       </div>
     </div>
   );
