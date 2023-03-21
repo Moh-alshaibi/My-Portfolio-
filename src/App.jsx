4d2c038540ea96418fb6d9ee6d0c98b4c08746c6
Moh-alshaibi <kib09005@gmail.com>
2023-03-21 02:55:44 +0100

    imports and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Aboutme from "./components/Aboutme";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -9,8 +13,18 @@ const App = () => {
         <img src="./src/assets/img/me.png" alt="" className="" />
         <div>
           <p className="ml-12 mt-4">Hey!</p>
-          <p className="ml-12 mt-4 w-96 ">My name is <span className="text-red-900">Mohamed</span> and I'm a web developer who specializes in designing and building web applications and websites.</p>
+          <p className="ml-12 mt-4 w-96 ">
+            My name is <span className="text-red-900">Mohamed</span> and I'm a
+            web developer who specializes in designing and building web
+            applications and websites.
+          </p>
         </div>
+      </div>
+      <div className="mt-96">
+        <Aboutme />
+        <Contact />
+        <Experience />
+        <Projects />
       </div>
     </div>
   );
