3baaa235de0cb396eaf3b91b59dbdc67dedce088
Moh-alshaibi <kib09005@gmail.com>
2023-03-31 18:39:29 +0200

    added HandleDiscoverClick

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Nav = () => {
+const Nav = ({ handleDiscoverClick }) => {
   return (
     <div className="flex flex-row justify-end mr-24">
       <nav className="mt-10 max-w-screen-lg">
         <ul className="flex flex-row justify-end">
-          <li className="bg-neutral-900 text-white font-bold py-2 px-4 rounded-md mr-4 cursor-pointer text-sm">
+          <li className="bg-neutral-900 text-white font-bold py-2 px-4 rounded-md mr-4 cursor-pointer text-sm" onClick={handleDiscoverClick}>
             Discover
           </li>
           <li className="bg-neutral-900 text-white font-bold py-2 px-4 rounded-md cursor-pointer text-sm">
