a740e43a390863795254a4f7b7d14f19911f7e52
Moh-alshaibi <kib09005@gmail.com>
2023-04-11 03:27:33 +0200

    Routed all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import React, { useRef } from "react";
 
 import Home from "./pages/Home";
-import Testing from "./pages/Testing";
+import Contacts from "./pages/Contacts";
+import Aboutme from "./pages/Aboutme";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
 
 const router = createBrowserRouter([
   {
@@ -11,7 +14,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/testing",
-    element: <Testing />,
+    element: <Aboutme />,
+  },
+  {
+    path: "/contact",
+    element: <Contacts/>,
+  },
+  {
+    path: "/experience",
+    element: <Experience />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
   },
 ]);
 
