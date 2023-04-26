6f96565b906bade04ac83629c7d2dcde0d3d49e1
Moh-alshaibi <kib09005@gmail.com>
2023-04-26 02:39:12 +0200

    added content to about me page

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const Blackfolder = () => {
   return (
     <div>
-      <p className="flex justify-center mt-96 text-4xl">About me</p>
+      <div>
+        <p className="absolute top-36 left-20 w-96 p-8">
+          Hello there! I am a 21-year-old front-end developer originally from
+          Yemen, but currently residing in Germany. My passion lies in the tech
+          industry, and my goal is to create exciting and innovative websites
+          for people to enjoy.
+        </p>
+        <p className="absolute top-96 left-20 mt-72 w-5/6 p-8" >
+          As a front-end developer, I am dedicated to crafting beautiful and
+          user-friendly websites that provide an exceptional user experience. I
+          am thrilled to be working in this exciting field and look forward to
+          what the future holds.
+        </p>
+      </div>
+      <div className="absolute bottom-10 right-10 text-xl">
+        <p>{"<About me/>"} </p>
+      </div>
+      <div className="absolute top-10 left-5 text-xl">
+        <p>{"<About me>"}</p>
+      </div>
     </div>
   );
 };
