b4250be9d7bfc49bafe632c0152214f68bbff268
Moh-alshaibi <kib09005@gmail.com>
2023-05-13 23:59:10 +0900

    added exit button function to the remaining files

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Experience = () => {
-    return (
+  const [showPage, setShowPage] = useState(true);
+
+  const handleClosePage = () => {
+    setShowPage(false);
+  };
+
+  return (
+    <>
+      {showPage && (
         <div>
-            <p className="flex justify-center mt-96 text-4xl">Experience</p>
+          <div>
+            
+            <Link
+              to="/home"
+              className="absolute top-8 right-10 text-3xl p-2 hover:text-yellow-900 cursor-pointer"
+              onClick={handleClosePage}
+            >
+              X
+            </Link>
+          </div>
+          <div className="absolute bottom-10 right-10 text-xl">
+            <p>{"<Experience/>"}</p>
+          </div>
+          <div className="absolute top-10 left-5 text-xl">
+            <p>{"<Experience>"}</p>
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default Experience;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Projects = () => {
-    return (
+const projects = () => {
+  const [showPage, setShowPage] = useState(true);
+
+  const handleClosePage = () => {
+    setShowPage(false);
+  };
+
+  return (
+    <>
+      {showPage && (
         <div>
-            <p className="flex justify-center mt-96 text-4xl">Projects</p>
+          <div>
+            
+            <Link
+              to="/home"
+              className="absolute top-8 right-10 text-3xl p-2 hover:text-yellow-900 cursor-pointer"
+              onClick={handleClosePage}
+            >
+              X
+            </Link>
+          </div>
+          <div className="absolute bottom-10 right-10 text-xl">
+            <p>{"<Projects/>"}</p>
+          </div>
+          <div className="absolute top-10 left-5 text-xl">
+            <p>{"<Projects>"}</p>
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
-export default Projects;
+export default projects;
