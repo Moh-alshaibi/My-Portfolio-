380bc0f9428192622697bd9fa189d8e463aa209f
Moh-alshaibi <kib09005@gmail.com>
2023-05-13 23:51:54 +0900

    added /home route as default

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import React, { useRef } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import React, { useRef } from 'react';
 
-import Home from "./pages/Home";
-import Contacts from "./pages/Contacts";
-import Aboutme from "./pages/Aboutme";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
+import Home from './pages/Home';
+import Contacts from './pages/Contacts';
+import Aboutme from './pages/Aboutme';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/home',
     element: <Home />,
   },
   {
-    path: "/aboutme",
+    path: '/aboutme',
     element: <Aboutme />,
   },
   {
-    path: "/contact",
-    element: <Contacts/>,
+    path: '/contact',
+    element: <Contacts />,
   },
   {
-    path: "/experience",
+    path: '/experience',
     element: <Experience />,
   },
   {
-    path: "/projects",
+    path: '/projects',
     element: <Projects />,
   },
+  {
+    path: '/',
+    element: <Navigate to='/home' replace={true} />,
+  },
 ]);
-
-
 
 const App = () => {
   return (
