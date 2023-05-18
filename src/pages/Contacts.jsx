aa6eaf251c86be454d4a719d7f3b8492424e393e
Moh-alshaibi <kib09005@gmail.com>
2023-05-18 11:23:59 +0900

    minor changes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -43,34 +43,37 @@ const Contacts = () => {
             <h2 className="flex justify-center text-white">Contact me</h2>
             <form onSubmit={handleSubmit} className="contact-form">
               <div className="form-group">
-                <label className="text-white" htmlFor="name">Name</label> 
+                <label className="text-white" htmlFor="name">
+                  Name
+                </label>
                 <input
                   type="text"
                   id="name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
-                
                   required
                 />
               </div>
               <div className="form-group">
-                <label className="text-white" htmlFor="email">Email</label>
+                <label className="text-white" htmlFor="email">
+                  Email
+                </label>
                 <input
                   type="email"
                   id="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  
                   required
                 />
               </div>
               <div className="form-group">
-                <label className="text-white" htmlFor="message">Message</label>
+                <label className="text-white" htmlFor="message">
+                  Message
+                </label>
                 <textarea
                   id="message"
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
-                
                   required
                 />
               </div>
